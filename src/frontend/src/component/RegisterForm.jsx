67b834c725cef2cc40782073102145dc6ae7c93b
favourdan <106870617+favourdan@users.noreply.github.com>
2023-04-18 16:28:30 +0100

    Initialized project

--- src/frontend/src/component/RegisterForm.jsx
+++ src/frontend/src/component/RegisterForm.jsx
@@ -3,15 +3,21 @@ import { useState } from 'react'
 
 const BASE_URL = "http://localhost:8080/api/v1"
 const RegisterForm = () => {
-    const [formData, setFormData] = useState({
-        userName: "",
-        fullName: "",
-        emailAddress: "",
-        phoneNumber: ""
-    })
+    const [username, setUsername] = useState("")
+    const [fullName, setFullName] = useState("")
+
+    const [emailAddress, setEmailAddress] = useState("")
+
+    const [phoneNumber, setPhoneNumber] = useState("")
+
 
     const handleRegistration = () => {
-        axios.post(`${BASE_URL}/register`, formData)
+        axios.post(`${BASE_URL}/register`, {
+            userName: userName,
+            fullName: fullName,
+            emailAddress: emailAddress,
+            phoneNumber: phoneNumber
+        })
             .then(res => {
                 console.log(res.data);
             })
@@ -20,10 +26,10 @@ const RegisterForm = () => {
             })
     }
 
-    const setFullName = (e) => setFormData(prevState => { prevState})
-    const setUsername = (e) => setFormData(prevState => {prevState, userName: e.target.value })
-    const setEmailAddress = (e) => setFormData({..., emailAddress: e.target.value })
-    const setPhoneNumber = (e) => setFormData({..., phoneNumber: e.target.value })
+    const handleSetFullName = (e) => setFullName(e.target.value)
+    const handleSetUsername = (e) => setUsername(e.target.value)
+    const handleSetEmailAddress = (e) => setEmailAddress(e.target.value)
+    const handleSetPhoneNumber = (e) => setPhoneNumber(e.target.value)
 
 
 
@@ -32,16 +38,16 @@ const RegisterForm = () => {
         <section>
             <form>
                 <label>Username
-                <input type="text" onChange={setUsername}/>
+                <input type="text" onChange={handleSetUsername}/>
                 </label>
                 <label>FullName
-                <input type="text" onChange={setFullName}/>
+                <input type="text" onChange={handleSetFullName}/>
                 </label>
                 <label>Email Address
-                <input type="email" onChange={setEmailAddress}/>
+                <input type="email" onChange={handleSetEmailAddress}/>
                 </label>
                 <label>Phone Number
-                <input type="number" onChange={setPhoneNumber}/>
+                <input type="number" onChange={handleSetPhoneNumber}/>
                 </label>
                 <button type={"submit"} onClick={handleRegistration}>REGISTER</button>
             </form>
